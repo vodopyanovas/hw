e99d4067276b6c2214e44fb1db2f288dfc481ab6
falcon178 <falcon178@gmail.com>
2016-09-08 13:16:25 +0300

    Done

--- homework1/homework1.py
+++ homework1/homework1.py
@@ -14,46 +14,53 @@ log = open('log.txt', 'a')
 
 name = input ('Привет! Это небольшая викторина по пройденной теме. Для начала игры введи своё имя \n> ')
 now = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")
-log.write('#######################################\n')
+log.write('######################################################################\n')
 log.write(now + ' ' + name + '\n')
 
 print ('\n{}, ответы указываются в виде одного слова/цифры/ на английском языке\n'.format(name))
 
+# старт игры
 run_game = 1
 while run_game == 1:
     question_num = 0
     right_answers = 0
     while question_num < 7:
         print (questions[question_num])    
-        usr_answer = input ('\n > ') 
+        usr_answer = input ('> ') 
         usr_answer = usr_answer.lower()
-        #now = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")	
-	    #log.write(now + ' ' + usr_answer + '\n')
-
+        now = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")	
+        log.write(now + ' ' + usr_answer + '\n')
+        
+        # проверка ответов
         if usr_answer == answers[question_num]: 
-            right_answers +=1        
+            right_answers += 1        
             print ('\nВерно!') 
-            print ('Количество правильных ответов: {}'.format(right_answers))
-    
+            print ('Количество правильных ответов: {}\n'.format(right_answers))
+
         else:
 	        print ('Неверно.')
+	        print ('Количество правильных ответов: {}\n'.format(right_answers))
 
-        question_num += 1
-        # конец цикла for
-
+        question_num += 1        
+    # конец игры, подведение результатов
     run_game = 0 
     print ('\n{0}, за игру вы дали правильных ответов: {1} из 7'.format(name, right_answers))
-
+    now = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")
+    log.write('\n')	
+    log.write(now + ' Right answers: ' + str(right_answers) + ' of 7\n')
+    log.write('______________________________________________________________________\n')
     
     gameover = input ('Для повторения игры нажмите 1, для выхода - 0 \n > ')
     if gameover == '1':
+    	now = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")
+    	log.write(now + ' ***New game*** \n')
     	run_game = 1
     elif gameover == '0':
         print ('\nСпасибо за игру!')
         break
     else:
     	print ('Нормально же попросил, ну! Не буду с тобой играть!')
-    # конец цикла while
+    
 
 
 
