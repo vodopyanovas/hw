0b27de4ef5561c3f6de5dc3f2dd4c5ca2bc4a04a
falcon178 <falcon178@gmail.com>
2016-09-10 12:16:30 +0300

    style improvements

--- homework1/homework1.py
+++ homework1/homework1.py
@@ -4,7 +4,7 @@ __author__ =    "Vodopyanov Anton"
 import sys
 
 # подключаем модуль для записи текущего времени в лог
-from datetime import datetime # подключаем модуль для записи текущего времени в лог
+from datetime import datetime 
 
 # оставил вопросы из лёгкой части задания закомменчеными
 ''' 
@@ -31,12 +31,12 @@ for line in quest_file:
 
 quest_file.close()
 
-name = input ('Привет! Это небольшая викторина по пройденной теме. Для начала игры введи своё имя \n> ')
+name = input('Привет! Это небольшая викторина по пройденной теме. Для начала игры введи своё имя \n> ')
 now = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")
 log.write('######################################################################\n')
 log.write(now + ' ' + name + '\n')
 
-print ('\n{}, ответы указываются в виде одного слова/цифры/ на английском языке\n'.format(name))
+print('\n{}, ответы указываются в виде одного слова/цифры/ на английском языке\n'.format(name))
 
 # старт игры
 run_game = 1
@@ -44,8 +44,8 @@ while run_game == 1:
     question_num = 0
     right_answers = 0
     while question_num < len(questions):
-        print (questions[question_num])    
-        usr_answer = input ('> ') 
+        print(questions[question_num])    
+        usr_answer = input('> ') 
         usr_answer = usr_answer.lower()
         now = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")	
         log.write(now + ' ' + usr_answer + '\n')
@@ -53,35 +53,31 @@ while run_game == 1:
         # проверка ответов
         if usr_answer == answers[question_num]: 
             right_answers += 1        
-            print ('\nВерно!') 
-            print ('Количество правильных ответов: {}\n'.format(right_answers))
+            print('\nВерно!') 
+            print('Количество правильных ответов: {}\n'.format(right_answers))
 
         else:
-	        print ('Неверно.')
-	        print ('Количество правильных ответов: {}\n'.format(right_answers))
+	        print('Неверно.')
+	        print('Количество правильных ответов: {}\n'.format(right_answers))
 
         question_num += 1        
     # конец игры, подведение результатов
     run_game = 0 
-    print ('\n{0}, за игру вы дали правильных ответов: {1} из {2}'.format(name, right_answers,len(questions)))
+    print('\n{0}, за игру вы дали правильных ответов: {1} из {2}'.format(name, right_answers,len(questions)))
     now = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")
     log.write('\n')	
     log.write(now + ' Right answers: ' + str(right_answers) + ' of '+ str(len(questions)) + '\n')
     log.write('______________________________________________________________________\n')
     
-    gameover = input ('Для повторения игры нажмите 1, для выхода - 0 \n > ')
+    gameover = input('Для повторения игры нажмите 1, для выхода - 0 \n > ')
     if gameover == '1':
     	now = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")
     	log.write(now + ' ***New game*** \n')
     	run_game = 1
     elif gameover == '0':
-        print ('\nСпасибо за игру!')
+        print('\nСпасибо за игру!')
         log.close()
         break
     else:
-    	print ('Нормально же попросил, ну! Не буду с тобой играть!')
+    	print('Нормально же попросил, ну! Не буду с тобой играть!')
     	log.close()
-    
-
-
-
