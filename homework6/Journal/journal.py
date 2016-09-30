6dd3fa730d94f822d3e9110b1b221dcde16232f6
Anton Vodopyanov <vodopyanovas@ya.ru>
2016-09-30 18:02:17 +0300

    journal v1.0

--- homework6/Journal/journal.py
+++ homework6/Journal/journal.py
@@ -19,8 +19,36 @@ class Journal (object):
             pickle.dump(self.marks, file)
 
     # searches particular pupil marks
-    def show_marks(self):
-        pass
+    def show_marks(self, name, last_name, school_class):
+        try:
+            with open('journal.dat', 'rb') as file:
+                load = pickle.load(file)
+
+        except:
+            print('\nNope :(\n\n')
+
+        external_loop_index = 0
+        internal_loop_index = 0
+
+        for list_element in load:
+            match_count = 0
+            for nested_element in list_element:
+                if name == nested_element:
+                    match_count += 1
+
+                if last_name == nested_element:
+                    match_count += 1
+
+                if school_class == nested_element:
+                    match_count += 1
+
+            if match_count == 3:
+                print(load[external_loop_index])
+                
+            else:
+                internal_loop_index += 1
+
+            external_loop_index += 1
 
     # loads all records from file
     def show_all_marks(self,):
@@ -44,7 +72,7 @@ class Journal (object):
     # input handling : will call a method depanding on user's input
     def input_parcer(self):
 
-        string = input('Please enter your command > ')
+        string = input('\nPlease enter your command > ')
         regexp1 = re.findall(r'(show).*(all).*(marks)', string)
         regexp2 = re.findall(r"(add)\s(\w+)\s(\w+).*(\d\w).*(\d)\s\w+?\s(\w+)", string)
         regexp3 = re.findall(r"(show)\s(\w+)\s(\w+).*?(\d\w)\s?(\w+)", string)
@@ -74,6 +102,9 @@ class Journal (object):
                 name = command[0][1]
                 last_name = command[0][2]
                 school_class = command[0][3]
+
+                self.show_marks(name,last_name,school_class)
+
                 # search a string in file which will contain: name, last_name and school_class - > print it
 
         except:
@@ -85,6 +116,15 @@ class Journal (object):
             else:
                 print('Wrong input!')
 
-journal = Journal([])
-journal.load_info()
-journal.input_parcer()
+
+def main():
+    journal = Journal([])
+    journal.load_info()
+
+    while True:
+        try:
+            journal.input_parcer()
+        except :
+            print('\n\n*** Shutting down, bye! *** ')
+            break
+main()
