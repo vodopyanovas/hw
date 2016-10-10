e3b7afa068cbf63050ffac48c36ead9b08cb254d
Anton Vodopyanov <vodopyanovas@ya.ru>
2016-10-10 23:24:05 +0300

    kind of blog with DB..

---
+++ homework9/config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Anton Vodopyanov'
+
+DEBUG = True
+SECRET_KEY = 'you-will-never-guess8==8'
+
+# Database settings:
+SQLALCHEMY_DATABASE_URI = 'sqlite:///blog.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
