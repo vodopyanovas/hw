958da5a5cebbb4bf33ed081f906b436129dc104b
Anton Vodopyanov <vodopyanovas@ya.ru>
2016-10-13 13:55:45 +0300

    nu takoe..

--- homework9/2/models.py
+++ homework9/2/models.py
@@ -7,33 +7,34 @@ __author__ = 'Anton Vodopyanov'
 
 class BuyAdvert(db.Model):
     __tablename__ = 'buy_advert'
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Float(6), nullable=False)
 
-    responses = db.relationship('AdvertResponse', backref='posts', lazy='dynamic')
+    advert_id = db.Column(db.Integer, db.ForeignKey('advert_response.id'))
+    responses = db.relationship('AdvertResponse', backref=db.backref('response_to', lazy='dynamic'))
 
-    def __str__(self):
-        return'<Advert %s>' % self.responses
-    #
-    # def __init__(self, title, price):
-    #     self.title = title
-    #     self.price = price
+    def __init__(self, title, price, responses):
+        self.title = title
+        self.price = price
+        self.responses = responses
+
+    # def __repr__(self):
+    #     return'<Advert %s>' % self.responses
 
 
 class AdvertResponse(db.Model):
     __tablename__ = 'advert_response'
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(300), nullable=False)
     phone = db.Column(db.String(15), nullable=False)
 
-    buyadvert_id = db.Column(db.Integer, db.ForeignKey('buy_advert.id'))
+    def __init__(self, text, phone, response_to):
+        self.text = text
+        self.phone = phone
+        self.response_to = response_to
 
 
-
-    # def __init__(self, text, phone):
-    #     self.text = text
-    #     self.phone = phone
 
 
 
